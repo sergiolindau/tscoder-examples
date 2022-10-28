9f046d7902b06a9b6acc546b84adbb42e3394f45
Sergio Lindau <sergiolindau@gmail.com>
2022-10-28 13:06:43 -0300

    Initial Commit

---
+++ example/01-simple-definitions-generation/README.md
@@ -0,0 +1,10 @@
+# ts-csv-json5
+A CSV and JSON5 tools for generate related TypeScript code.
+
+## Packages
+
+- [ts-morph](https://www.npmjs.com/package/ts-morph) - TypeScript Compiler API wrapper. Provides an easier way to programmatically navigate and manipulate TypeScript and JavaScript code.
+
+## Resources
+
+- [TypeScript AST Viewer](https://ts-ast-viewer.com)
---
+++ example/01-simple-definitions-generation/src/index.ts
@@ -0,0 +1,35 @@
+const timeWas = performance.now()
+
+console.log('Synchronous pipeline start.')
+
+const timeRun = (): string => {
+    return `${((performance.now() - timeWas)/1e3).toString().substring(0,5)}s`
+}
+
+/* Synchronous test run */
+; (() => {
+
+    console.log('========================================================')
+    console.log('=====           Synchronous test start             =====')
+    console.log('========================================================')
+
+
+    console.log('Synchronous function finished in', timeRun())
+    console.log('========================================================')
+
+})();
+
+/* Asynchronous test run */
+; (async () => {
+
+    console.log('========================================================')
+    console.log('=====           Asynchronous test start            =====')
+    console.log('========================================================')
+
+
+    console.log('Asynchronous function finished in', timeRun())
+    console.log('========================================================')
+
+})();
+
+console.log('Synchronous pipeline finished in', timeRun())
